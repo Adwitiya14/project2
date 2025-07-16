ec75a23f60d562dec0d54a60fdf4ceb795cfcc0f
Adwitiya14 <its.adwitiya1411@gmail.com>
2025-07-16 23:37:01 +0530

    my commit

---
+++ circlearea.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area;
+    float pi = 3.14159;
+
+    // Ask user to enter the radius
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    // Calculate area
+    area = pi * radius * radius;
+
+    // Display the result
+    printf("Area of the circle = %.2f\n", area);
+
+    return 0;
+}
