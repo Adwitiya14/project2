0e298a8c27c0cfd900dd15ed773496bfe3bf63a9
Adwitiya14 <its.adwitiya1411@gmail.com>
2025-07-17 17:01:41 +0530

    my commit

---
+++ intfloat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 60;
+    float b = 3.3;
+    float result;
+
+    result = a + b;
+
+    printf("Integer: %d", a);
+    printf("Float: %.2f", b);
+    printf("Sum (int + float): %.2f", result);
+
+    return 0;
+}
